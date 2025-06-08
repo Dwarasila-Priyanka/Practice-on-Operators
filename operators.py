b8104b82d585e8994328e6da17d9de34d8c1ba6e
Dwarasila Priyanka <dwarasilapriyanka123@gmail.com>
2025-06-08 12:52:23 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,83 @@
+# 1. Arithmetic Operators
+
+a = 10
+b = 5
+print("Sum:", a + b) # Sum: 15
+print("Difference:", a - b) # Difference: 5
+print("Product:", a * b) # Product: 50
+print("Divison:", a / b) # Division : 2.0
+print("Floor Division:", a // b) # Floor Division: 2
+print("Modulus:", a % b) # Modulus: 0
+print("Exponent:", a ** 2) # Exponent: 100
+
+# 2. Comparison Operators
+
+x = 10
+y = 15
+print("x == y:", x == y) # x == y: False
+print("x != y:", x != y) # x != y: True
+print("x > y:", x > y) # x > y: False
+print("x < y:", x < y) # x < y: True
+print("x >= y:", x >= y) # x >= y: False
+print("x <= y:", x <= y) # x <= y: True
+
+# 3. Logical Operators
+
+a = True
+b = False
+print("a and b:", a and b) # a and b: False
+print("a or b:", a or b) # a or b: True
+print("not a:", not a) # not a: False
+print("not b:", not b) # not b: True
+
+ # 4. Assignment Operators
+
+x = 10
+print("Initial x:", x)  # Initial x: 10
+x += 5
+print("After x += 5:", x) # After x += 5: 15
+x *= 2
+print("After x *= 2:", x) # After x *= 2: 30
+x -= 3
+print("After x -= 3:", x) # After x -= 3: 7
+x /= 4
+print("After x /= 4:", x) # After x /= 4: 2.5
+
+# 5. Bitwise Operators
+
+a = 5   # 0101 in binary
+b = 3   # 0011 in binary
+print("a & b:", a & b)    # a & b: 1
+print("a | b:", a | b)    # a | b: 7
+print("a ^ b:", a ^ b)    # a ^ b: 6
+print("~a:", ~a)          # ~a: -6
+print("a << 1:", a << 1)  # a << 1: 10
+print("a >> 1:", a >> 1)  # a >> 1: 2
+
+# 6. Membership Operators
+
+my_list = [10, 20, 30]
+print( 20 in my_list) # True
+print( 50 not in my_list) # True
+
+text = "hello"
+print('e' in text) # True
+print( 'h' not in text) # False
+
+# 7. Identity Operators
+
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print("a is b:", a is b) # a is b: True
+print("a is c:", a is c) # a is c: False
+print("a == c:", a == c) # a == c: True
+print("a is not c:", a is not c) # a is not c: True
+
+
+
+
+ 
+
+
